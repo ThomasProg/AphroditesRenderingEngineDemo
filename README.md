b44c97d6632c4d8df5a4c11f32268a5e8270afe5
Prog'z <39605375+ThomasProg@users.noreply.github.com>
2022-03-09 17:52:24 +0100

    Demo project added

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # AphroditesRenderingEngineDemo
+
+cmake -B Build/Debug
+
+cmake --build Build/Debug --config Debug --target AphroditesRenderingEngine AphroditesRenderingEngineDemo
+
+./Build/Debug/Demo/Debug/Demo.exe
---
+++ src/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+#include <Context.hpp>
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+
+#include <assimp/Importer.hpp>      // C++ importer interface
+#include <assimp/scene.h>           // Output data structure
+#include <assimp/postprocess.h>     // Post processing flags
+
+
+static void onError(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+ 
+static void onInput(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+ 
+int main()
+{
+    glfwSetErrorCallback(onError);
+ 
+    if (!glfwInit())
+    {
+        std::cout << "glfw couldn't init" << std::endl;
+        return EXIT_FAILURE;
+    }
+ 
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+    GLFWwindow* window = glfwCreateWindow(600, 500, "Aphrodite's Rendering Engine Demo", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        std::cout << "glfw couldn't create window" << std::endl;
+        return EXIT_FAILURE;
+    }
+ 
+    glfwSetKeyCallback(window, onInput);
+
+    {
+        // Create an instance of the Importer class
+        Assimp::Importer importer;
+
+        // And have it read the given file with some example postprocessing
+        // Usually - if speed is not the most important aspect for you - you'll
+        // probably to request more postprocessing than we do in this example.
+        const aiScene* scene = importer.ReadFile("",
+            aiProcess_CalcTangentSpace       |
+            aiProcess_Triangulate            |
+            aiProcess_JoinIdenticalVertices  |
+            aiProcess_SortByPType);
+        if (scene == nullptr)
+            std::cout << "Scene could not be loaded" << std::endl;
+
+        ARE::Context context; 
+
+        glm::mat4 m{};
+        std::cout << "Length : " << m.length() << std::endl;
+
+        while (!glfwWindowShouldClose(window))
+        {
+
+            glfwPollEvents();
+        }
+    }
+ 
+    glfwDestroyWindow(window);
+ 
+    glfwTerminate();
+    return EXIT_SUCCESS;
+}
+ 
